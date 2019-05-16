d6c1cf7852181f2eff5f7b049066acb86511fd4c
caiyujie <16210720048@fudan.edu.cn>
2019-05-16 23:43:08 +0800

    caiyujie

---
+++ train/mnist_int16.py
@@ -0,0 +1,119 @@
+# -*- coding: utf-8 -*-
+import input_data
+import tensorflow as tf
+import numpy as np
+from tf_fix import *
+
+mnist = input_data.read_data_sets('MNIST_data', one_hot=True)
+sess = tf.InteractiveSession()
+
+with tf.name_scope('input'): 
+	x = tf.placeholder("float", shape=[None, 784])
+	y_ = tf.placeholder("float", shape=[None, 10])
+
+def weight_variable(shape):
+	initial = tf.truncated_normal(shape, stddev=0.1);
+	return tf.Variable(initial)
+
+def bias_variable(shape):
+	initial = tf.constant(0.1, shape=shape)
+	return tf.Variable(initial)
+
+def conv2d(x, W):
+	return tf.nn.conv2d(x, W, strides=[1, 1, 1, 1], padding='SAME')
+
+def max_pool_2x2(x):
+	return tf.nn.max_pool(x, ksize=[1, 2, 2, 1], strides=[1, 2, 2,1], padding='SAME')
+
+#First Convolutional Layer
+with tf.name_scope('1st_CNN'): 
+	W_conv1 = weight_variable([5, 5, 1, 32])
+	b_conv1 = bias_variable([32])
+	x_image = tf.reshape(x, [-1,28,28,1])
+	h_conv1_before_lut = conv2d(x_image, W_conv1)+b_conv1
+	h_conv1 = tf.nn.sigmoid( h_conv1_before_lut )
+	h_pool1 = max_pool_2x2(h_conv1)
+
+#Second Convolutional Layer
+with tf.name_scope('2rd_CNN'): 
+	W_conv2 = weight_variable([5, 5, 32, 64])
+	b_conv2 = bias_variable([64])
+	h_conv2_before_lut = conv2d(h_pool1, W_conv2)+b_conv2
+	h_conv2 = tf.nn.sigmoid( h_conv2_before_lut )
+	h_pool2 = max_pool_2x2(h_conv2)
+
+#Densely Connected Layer
+with tf.name_scope('Densely_NN'): 
+	W_fc1 = weight_variable([ 7* 7* 64, 1024])
+	b_fc1 = bias_variable([1024])
+	h_pool2_flat = tf.reshape(h_pool2, [-1, 7*7*64])
+	h_fc1_before_lut = tf.matmul(h_pool2_flat , W_fc1)+b_fc1
+	h_fc1=tf.nn.sigmoid( h_fc1_before_lut )
+
+#Dropout
+with tf.name_scope('Dropout'):
+	keep_prob = tf.placeholder("float")
+	h_fc1_drop = tf.nn.dropout(h_fc1, keep_prob)
+
+#Readout Layer
+with tf.name_scope('Softmax'):
+	W_fc2 = weight_variable([1024, 10])
+	b_fc2 = bias_variable([10])
+	h_fc2 = tf.matmul(h_fc1_drop, W_fc2) + b_fc2
+	y_conv=tf.nn.softmax(h_fc2)
+
+with tf.name_scope('Loss'):
+	cross_entropy = -tf.reduce_sum(y_*tf.log(y_conv))
+
+with tf.name_scope('Train'):
+	train_step = tf.train.AdamOptimizer(1e-3).minimize(cross_entropy)
+	#train_step = tf.train.AdamOptimizer(5e-5).minimize(cross_entropy)
+
+with tf.name_scope('Accuracy'):
+	correct_prediction = tf.equal(tf.argmax(y_conv ,1), tf.argmax(y_,1))
+	accuracy = tf.reduce_mean(tf.cast(correct_prediction , "float"))
+
+#merged = tf.merge_all_summaries();
+#writer = tf.train.SummaryWriter("logs/",sess.graph) 
+
+tf.initialize_all_variables().run()
+
+for i in range(2000):#000):
+	batch = mnist.train.next_batch(200);
+	if i%200 == 0:
+		train_accuracy = accuracy.eval(feed_dict={x:batch[0], y_: batch[1], keep_prob:1.0});
+		print("step %d, training accuracy %g"%(i, train_accuracy));
+	train_step.run(feed_dict={x: batch[0], y_: batch[1], keep_prob:0.5});
+
+print("test accuracy %g"%accuracy.eval(feed_dict={x: mnist.test.images, y_: mnist.test.labels, keep_prob: 1.0}))
+
+f_cfg = open('./record/MNIST_LARGE_cfg.h', 'w')
+Record_Conv_Cfg(28,28,1,32,5,5,1,1,2,2,2,2,"conv1",f_cfg);
+Record_Conv_Cfg(14,14,32,64,5,5,1,1,2,2,2,2,"conv2",f_cfg);
+Record_Conv_Cfg(7,7,64,1024,7,7,1,1,0,0,0,0,"fc1",f_cfg);
+Record_Conv_Cfg(1,1,1024,10,1,1,1,1,0,0,0,0,"fc2",f_cfg);
+	
+Get_Feature_Fraction_Part(x,"img",{x: mnist.test.images, y_: mnist.test.labels, keep_prob: 1.0},f_cfg)
+Record_Weight(W_conv1,"W_conv1",f_cfg)
+Record_Bias(b_conv1,"b_conv1",f_cfg)
+Get_Feature_Fraction_Part(h_conv1_before_lut,"h_conv1_before_lut",{x: mnist.test.images, y_: mnist.test.labels, keep_prob: 1.0},f_cfg)
+Get_Feature_Fraction_Part(h_conv1,"h_conv1",{x: mnist.test.images, y_: mnist.test.labels, keep_prob: 1.0},f_cfg)
+Get_Feature_Fraction_Part(h_pool1,"h_pool1",{x: mnist.test.images, y_: mnist.test.labels, keep_prob: 1.0},f_cfg)
+	
+Record_Weight(W_conv2,"W_conv2",f_cfg)
+Record_Bias(b_conv2,"b_conv2",f_cfg)
+Get_Feature_Fraction_Part(h_conv2_before_lut,"h_conv2_before_lut",{x: mnist.test.images, y_: mnist.test.labels, keep_prob: 1.0},f_cfg)
+Get_Feature_Fraction_Part(h_conv2,"h_conv2",{x: mnist.test.images, y_: mnist.test.labels, keep_prob: 1.0},f_cfg)
+Get_Feature_Fraction_Part(h_pool2,"h_pool2",{x: mnist.test.images, y_: mnist.test.labels, keep_prob: 1.0},f_cfg)
+	
+Record_Weight(tf.reshape(W_fc1,[7,7,64,1024]),"W_fc1",f_cfg)
+Record_Bias(b_fc1,"b_fc1",f_cfg)
+Get_Feature_Fraction_Part(h_fc1_before_lut,"h_fc1_before_lut",{x: mnist.test.images, y_: mnist.test.labels, keep_prob: 1.0},f_cfg)
+Get_Feature_Fraction_Part(h_fc1,"h_fc1",{x: mnist.test.images, y_: mnist.test.labels, keep_prob: 1.0},f_cfg)
+	
+Record_Weight(tf.reshape(W_fc2,[1,1,1024,10]),"W_fc2",f_cfg)
+Record_Bias(b_fc2,"b_fc2",f_cfg)
+Get_Feature_Fraction_Part(h_fc2,"h_fc2",{x: mnist.test.images, y_: mnist.test.labels, keep_prob: 1.0},f_cfg)		
+f_cfg.close();
+
+sess.close()
---
+++ train/tf_fix.py
@@ -0,0 +1,204 @@
+# -*- coding: utf-8 -*-
+import input_data
+import tensorflow as tf
+import numpy as np
+import math
+import struct
+
+Tc=32
+Tk=16
+Tp=16
+Logic_MEM_DEP=256
+Logic_MEM_NUM=16
+
+BIT_WIDTH=16;
+
+def Get_FeatureLength(H,W,CH):
+	return (Tk*H*W*((CH+Tk-1)/Tk))
+
+def Get_WeightLength(Ky,Kx,CHin,CHout):
+	return (Tc*Kx*Ky*CHout*((CHin+Tc-1)/Tc))
+
+def To_Fixed(tensor,bitwidth):
+	array=tensor.eval();
+	range=max(np.max(array),-np.min(array))
+	int_part=max(math.ceil(math.log(range,2)+0.000001),0) + 1 #1 bit for sign
+	fraction_part=bitwidth-int_part
+	return ( np.round(array*pow(2,fraction_part)) , fraction_part ) #/pow(2,fraction_part) 
+
+def Feature_To_Fixed(tensor,bitwidth,feed_dict):
+	array=tensor.eval(feed_dict=feed_dict);
+	range=max(np.max(array),-np.min(array))
+	#print range;
+	int_part=max(math.ceil(math.log(range,2)+0.000001),0) + 1 #1 bit for sign
+	fraction_part=bitwidth-int_part
+	return ( np.round(array*pow(2,fraction_part)) , fraction_part ) #/pow(2,fraction_part) 
+
+def Map_Weight_Data(kernel,mem,Ky,Kx,in_ch,out_ch):
+	addr=0;
+	for k in range(0,out_ch,Tk):
+		for l in range(0,in_ch,Tc):
+			for i in range(Ky):
+				for j in range(Kx):
+					for kk in range(k,k+Tk):
+						if(kk<out_ch):
+							for ll in range(l,l+Tc,Tk):
+								tp=[];
+								for lll in range(ll,ll+Tk):
+									if lll<in_ch:
+										tp.append(kernel[i][j][lll][kk]);#kernel[i*Kx*in_ch*out_ch+j*in_ch*out_ch+lll*out_ch+kk]);
+									else:
+										tp.append(0);
+								for cp in range(Tk):
+									#print("k:"+str(k)+",l:"+str(l)+",i:"+str(i)+",j:"+str(j)+",kk:"+str(kk)+",ll:"+str(ll)+",lll:"+str(lll)+":"+str(addr+cp));
+									mem[addr+cp]=tp[cp];
+								addr=addr+Tk;
+
+def Map_Bias_Data(dat,mem,channel):
+	for i in range(0,channel,Tk):
+		for ii in range(i,i+Tk):	
+			if(ii<channel):
+				mem[ii]=dat[ii];
+			else:
+				mem[ii]=0;
+
+def Get_Feature_Fraction_Part(tensor,name,feed_dict,file):
+	(array,fraction_part)=Feature_To_Fixed(tensor,BIT_WIDTH,feed_dict);
+	file.write("#define %s %d\n" % ("PTR_"+name.upper(),int(fraction_part)) );
+	#print(name+' fraction_part: ' + str(int(fraction_part)));
+
+def Record_Weight(tensor,name,file):
+	(array,fraction_part)=To_Fixed(tensor,BIT_WIDTH);
+	file.write("#define %s %d\n" % ("PTR_"+name.upper(),int(fraction_part)) );
+	#print(name+' fraction_part: ' + str(fraction_part));
+	OneD_array_size=Get_WeightLength(np.shape(array)[0],np.shape(array)[1],np.shape(array)[2],np.shape(array)[3]);
+	OneD_array=[0]*OneD_array_size;
+	Map_Weight_Data(array,OneD_array,np.shape(array)[0],np.shape(array)[1],np.shape(array)[2],np.shape(array)[3]);
+	print("struct Mapped_Weight *%s=Malloc_Weight(%d,%d,%d,%d,%s);" % (name,np.shape(array)[0],np.shape(array)[1],np.shape(array)[2],np.shape(array)[3],"PTR_"+name.upper()) )
+	print("Load_Weight_From_File(%s,\"%s\");\n" % (name,name+'.bin') );
+	with open('./record/'+name+'.bin', 'wb') as fp:
+		for i in range(OneD_array_size):
+			a=struct.pack('h',OneD_array[i])
+			fp.write(a)
+
+def Record_Bias(tensor,name,file):
+	(array,fraction_part)=To_Fixed(tensor,BIT_WIDTH);
+	file.write("#define %s %d\n" % ("PTR_"+name.upper(),int(fraction_part)) );
+	#print(name+' fraction_part: ' + str(fraction_part));
+	OneD_array_size=Get_FeatureLength(1,1,np.shape(array)[0]);
+	OneD_array=[0]*OneD_array_size;
+	Map_Bias_Data(array,OneD_array,np.shape(array)[0]);
+	print("struct Mapped_Feature *%s=Malloc_Feature(1,1,%d,%s,0,-1,-1);" % (name,np.shape(array)[0],"PTR_"+name.upper()) )
+	print("Load_Feature_From_File(%s,\"%s\");\n" % (name,name+'.bin') );
+	with open('./record/'+name+'.bin', 'wb') as fp:
+		for i in range(OneD_array_size):
+			a=struct.pack('h',OneD_array[i])
+			fp.write(a)
+
+def Record_Conv_Cfg(Hin,Win,CHin,CHout,Kx,Ky,Sx,Sy,pad_left,pad_right,pad_up,pad_down,layername,file):
+	mininum_bw=0;
+	out_width=((Win+pad_left+pad_right-Kx)/Sx+1);
+	out_height=((Hin+pad_up+pad_down-Ky)/Sy+1);
+	overlap=Ky-Sy;
+	entries_per_line=Win*((CHin+Tc-1)/Tc);
+
+	dat_banks_restrict=(entries_per_line*Ky+Logic_MEM_DEP-1)/Logic_MEM_DEP;
+	wt_banks_restrict=(Kx*Ky*Tk*((CHin+Tc-1)/Tc)+Logic_MEM_DEP-1)/Logic_MEM_DEP;
+	if((dat_banks_restrict+wt_banks_restrict)>Logic_MEM_NUM):
+		printf("Error: CBUF entries not enough, you should split your "+layername+" into at least "+str((dat_banks_restrict+wt_banks_restrict)/Logic_MEM_NUM)+" pieces!!!\n");
+		return 
+
+	for dat_buf_num in range(dat_banks_restrict,Logic_MEM_NUM-wt_banks_restrict):
+		wt_banks=Logic_MEM_NUM-dat_buf_num;
+		out_ch_slice=( (Logic_MEM_DEP*wt_banks)/(Kx*Ky*Tk*((CHin+Tc-1)/Tc)) ) *Tk;
+
+		if(out_ch_slice>=CHout):
+			out_ch_slice=CHout;
+			N=1;
+		else:
+			N=(CHout+out_ch_slice-1)/out_ch_slice;
+
+		if(CHout%out_ch_slice==0):
+			out_ch_slice_last=out_ch_slice;
+		else:
+			out_ch_slice_last=CHout%out_ch_slice;
+
+		out_height_first=((Logic_MEM_DEP*dat_buf_num)/entries_per_line+pad_up-Ky)/Sy+1;
+		in_height_first=(out_height_first-1)*Sy+Ky-pad_up;
+
+		out_height_middle=((Logic_MEM_DEP*dat_buf_num)/entries_per_line-Ky)/Sy+1;
+		in_height_middle=(out_height_middle-1)*Sy+Ky;
+
+		if(out_height_first>=out_height):
+			out_height_first=out_height;
+			in_height_first=Hin;
+
+		if((out_height-out_height_first)%out_height_middle == 0):
+			K=(out_height-out_height_first)/out_height_middle+1;
+			out_height_last=out_height_middle;
+		else:
+			K=(out_height-out_height_first)/out_height_middle+2;
+			out_height_last=(out_height-out_height_first)%out_height_middle;
+
+		in_height_last=Hin-in_height_first+overlap-(K-2)*(in_height_first-overlap);
+
+		total_bw_K_to_N=(entries_per_line*Hin+entries_per_line*overlap*(K-1))*N+Kx*Ky*CHout*((CHin+Tc-1)/Tc);
+		total_bw_N_to_K=K*Kx*Ky*CHout*((CHin+Tc-1)/Tc)+entries_per_line*Hin+entries_per_line*overlap*(K-1);
+
+		if((mininum_bw==0) or (total_bw_K_to_N<mininum_bw)):
+			best_dat_banks=dat_buf_num;
+			mininum_bw=total_bw_K_to_N;
+			best_method=0;
+
+		if((mininum_bw==0) or (total_bw_N_to_K<mininum_bw)):
+			best_dat_banks=dat_buf_num;
+			mininum_bw=total_bw_N_to_K;
+			best_method=1;
+
+	dat_buf_num=best_dat_banks;
+	wt_banks=Logic_MEM_NUM-dat_buf_num;
+	out_ch_slice=( (Logic_MEM_DEP*wt_banks)/(Kx*Ky*Tk*((CHin+Tc-1)/Tc)) ) *Tk;
+
+	if(out_ch_slice>=CHout):
+		out_ch_slice=CHout;
+		N=1;
+	else:
+		N=(CHout+out_ch_slice-1)/out_ch_slice;
+
+	if(CHout%out_ch_slice==0):
+		out_ch_slice_last=out_ch_slice;
+	else:
+		out_ch_slice_last=CHout%out_ch_slice;
+
+	out_height_first=((Logic_MEM_DEP*dat_buf_num)/entries_per_line+pad_up-Ky)/Sy+1;
+	in_height_first=(out_height_first-1)*Sy+Ky-pad_up;
+
+	out_height_middle=((Logic_MEM_DEP*dat_buf_num)/entries_per_line-Ky)/Sy+1;
+	in_height_middle=(out_height_middle-1)*Sy+Ky;
+
+	if(out_height_first>=out_height):
+		out_height_first=out_height;
+		in_height_first=Hin;
+
+	if((out_height-out_height_first)%out_height_middle == 0):
+		K=(out_height-out_height_first)/out_height_middle+1;
+		out_height_last=out_height_middle;
+	else:
+		K=(out_height-out_height_first)/out_height_middle+2;
+		out_height_last=(out_height-out_height_first)%out_height_middle;
+
+	in_height_last=Hin-in_height_first+overlap-(K-2)*(in_height_first-overlap);
+
+	file.write("struct Conv_Cfg %s={%d,%d,%d,%d,%d,%d,%d,%d,%d,%d,%d,%d,%d,%d,%d,%d,%d,%d,%d,%d,%d,%d,%d,%d,%d,%d};\n" % (layername+"_cfg",
+	CHin,Win,CHout,
+	overlap,Kx,Ky,Sx,Sy,pad_left,pad_up,
+	best_dat_banks,best_method,
+	out_width,out_height,
+	entries_per_line,(Tc*2*Kx*Ky*CHout*((CHin+Tc-1)/Tc)),
+	K,
+	in_height_first,in_height_middle,in_height_last,
+	out_height_first,out_height_middle,out_height_last,
+	N,
+	out_ch_slice,
+	out_ch_slice_last));
+
